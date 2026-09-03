4825a909c026ddd5d4fd6a0e629c5645eef0048b
ronaldovenancio <ronaldovenanciorvs@gmail.com>
2026-09-03 14:39:42 -0300

    Adding lesson 1 assignment code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Emily Birch');
+});
+
+
+app.get('/hannah', (req, res) => {
+    res.send('Hannah Birch');
+});
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || 3000));
