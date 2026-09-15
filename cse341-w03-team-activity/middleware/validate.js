9af2182e4a0b1913168f5e57f6cd9f843eb6bbe7
ronaldovenancio <ronaldovenanciorvs@gmail.com>
2026-09-15 20:59:17 -0300

    Complete W03 team activity validation and error handling

---
+++ cse341-w03-team-activity/middleware/validate.js
@@ -0,0 +1,27 @@
+const validator = require('../helpers/validate');
+
+const saveContact = (req, res, next) => {
+  const validationRule = {
+    firstName: 'required|string',
+    lastName: 'required|string',
+    email: 'required|email',
+    favoriteColor: 'required|string',
+    birthday: 'string'
+  };
+
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      return res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    }
+
+    next();
+  });
+};
+
+module.exports = {
+  saveContact
+};
